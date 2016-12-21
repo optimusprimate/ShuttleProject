9f38c8d73b1ec66ecf29b430ba81618327db156b
Your Name <you@example.com>
2016-12-21 13:25:29 +0000

    Fourth commit

--- client/main.js
+++ client/main.js
@@ -4,56 +4,90 @@ import { Locations } from '../shared/locations.js';
 
 import './main.html';
 
-
 Router.configure({
     layoutTemplate: 'layout'
 });
 
 Router.route('/', function () {
-  this.render('shuttle');
-});
-
-Router.route('/spectator', function () {
   this.render('spectator');
 });
 
-Template.locator.onCreated(function locatorOnCreated() {
-    this.geoloc = new ReactiveVar({});
-    var _geoloc = this.geoloc;
-    this.timeid = setInterval(function() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(pos) {
-              pos = {type:'Point',coordinates:[pos.coords.latitude, pos.coords.longitude]};
-              Meteor.call('update.pos', 'shuttle', pos);
-              _geoloc.set(pos);
-            }, function(error) {
-                var errStr = "";
-                switch(error.code) {
-                    case error.PERMISSION_DENIED:
-                        errStr = "User denied the request for Geolocation."
-                        break;
-                    case error.POSITION_UNAVAILABLE:
-                        errStr = "Location information is unavailable."
-                        break;
-                    case error.TIMEOUT:
-                        errStr = "The request to get user location timed out."
-                        break;
-                    case error.UNKNOWN_ERROR:
-                        errStr = "An unknown error occurred."
-                        break;
-                }
-                Template.instance().geoloc.set({error: errStr});
-                
-            });
-        } else {
-            Template.instance().geoloc.set({error: "Geolocation is not supported by this browser."});
-        }
-    }, 1000);
+Router.route('/shuttle', function () {
+  this.render('shuttle');
 });
 
-Template.locator.helpers({
+var OneSignal = window.OneSignal || [];
+OneSignal.push(["init", {
+  appId: "bca68e0e-2b2b-4270-82e0-e254e9e2069a",
+  autoRegister: true,
+  notifyButton: {
+    enable: false
+  }
+}]);
+
+var geoloc = new ReactiveVar({});
+
+Template.locationDisplay.helpers({
   location() {
-    return JSON.stringify(Template.instance().geoloc.get());
+    return JSON.stringify(geoloc.get());
   },
 });
 
+
+function sendLocation(name) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      pos = {type:'Point',coordinates:[pos.coords.latitude, pos.coords.longitude]};
+      Meteor.call('update.pos', name, pos);
+      geoloc.set(pos);
+    }, function(error) {
+        var errStr = "";
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                errStr = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                errStr = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                errStr = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                errStr = "An unknown error occurred."
+                break;
+        }
+        geoloc.set({error: errStr});
+    });
+  } else {
+    geoloc.set({error: "Geolocation is not supported by this browser."});
+  }
+}
+
+Template.shuttle.onCreated(function () {
+  this.timeid = setInterval(function() {
+    sendLocation('shuttle');
+  }, 1000);
+});
+
+function sendSpectatorLocation() {
+  OneSignal.getUserId(function(userId) {
+    sendLocation(userId);
+  });
+}
+
+
+Template.spectator.onCreated(function () {
+  OneSignal.push(function() {
+    OneSignal.on('subscriptionChange', function (isSubscribed) {
+        if(isSubscribed) {
+          sendSpectatorLocation();
+        }
+    });
+  });
+});
+
+Template.spectator.events({
+  'click button'(event, instance) {
+    sendSpectatorLocation();
+  }
+});
--- server/main.js
+++ server/main.js
@@ -5,15 +5,29 @@ Meteor.startup(() => {
   // Locations.ensureIndex( { pos : "2dsphere" } );
 });
 
-/*
-  db.locations.update({name:'shuttle'},{$set:{pos:{type:'Point',coordinates:[30,40]}}})
-  db.locations.update({name:'spectator'},{$set:{pos:{type:'Point',coordinates:[31,40]}}})
-*/
+var ALERT_RADIUS = 10  /* km */ / 6378.1 /* degrees to km constant */;
+var NOTIFICATION_INTERVAL = 60000; // im ms
+var SHUTTLE_NAME = 'shuttle';
 
 Meteor.methods({
   'update.pos'(name, pos) {
     Locations.update({name}, {'$set': {pos}}, {upsert: true});
-    // db.locations.find({pos: { $geoWithin: { $center: [ [30, 40], 10 ]}}});
-    // push notification here
+    if(name == SHUTTLE_NAME) {
+      var people = Locations.find({pos: { $geoWithin: { $center: [ pos.coordinates, ALERT_RADIUS ]}}, name: {$ne: SHUTTLE_NAME}}).fetch();
+      for (var useridx in people) {
+        var user = people[useridx];
+        var currentTime = new Date();
+        if(typeof(user.lastNotificationDate) == 'undefined' || currentTime - user.lastNotificationDate > NOTIFICATION_INTERVAL) {
+          if(user.name != null) {
+            Locations.update(user._id, {'$set': {lastNotificationDate: currentTime}}, {upsert: true});
+            OneSignal.Notifications.create([user.name], {
+              contents: {
+                en: 'Shuttle is near...',
+              },
+            });
+          }
+        }
+      }
+    }
   }
 });
